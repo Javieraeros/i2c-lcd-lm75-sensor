5df236ae01fb375e40a9325feabe32e50321d051
Fco Javier Ruiz Rodríguez <fjruiz.1992@gmail.com>
2022-05-02 23:19:36 +0200

    Fix bad string for lm75 sensor

--- temp.py
+++ temp.py
@@ -1,14 +1,11 @@
 import I2C_LCD_driver
 from time import *
-import subprocess
+from subprocess import check_output
 from gpiozero import CPUTemperature
 
 def get_temp():
-    process = subprocess.Popen(['ssh', 'javi@zero2pi', 'sh', 'scripts/lm75.sh'],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    return stdout
+    process = check_output(['ssh', 'javi@zero2pi', 'sh', 'scripts/lm75.sh'])
+    return process.decode('utf-8')
 
 mylcd = I2C_LCD_driver.lcd()
 
@@ -16,6 +13,7 @@ while True:
     cpu = str(round(CPUTemperature().temperature, 1)).replace("\n", "") + " C"
     internal = "Internal: {}".format(cpu)
     external = "External: {}".format(get_temp())
+    print(get_temp())
     mylcd.lcd_display_string(internal, 1)
     mylcd.lcd_display_string(external, 2)
     sleep(5)

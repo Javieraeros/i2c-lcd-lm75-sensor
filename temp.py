703212d0a318447e1890ee69ed5317c5a236be0c
Fco Javier Ruiz Rodríguez <fjruiz.1992@gmail.com>
2022-11-16 15:47:16 +0100

    REturn error when there is an SSH error

--- temp.py
+++ temp.py
@@ -4,8 +4,12 @@ from subprocess import check_output
 from gpiozero import CPUTemperature
 
 def get_temp():
-    process = check_output(['ssh', 'javi@zero2pi', 'sh', 'scripts/lm75.sh'])
-    return process.decode('utf-8')
+    try:
+        process = check_output(['ssh', 'javi@zero2pi', 'sh', 'scripts/lm75.sh'])
+        result = process.decode('utf-8')
+    except:
+        result = "Error ssh"
+    return result
 
 mylcd = I2C_LCD_driver.lcd()
 
@@ -13,7 +17,7 @@ while True:
     cpu = str(round(CPUTemperature().temperature, 1)).replace("\n", "") + " C"
     internal = "Internal: {}".format(cpu)
     external = "External: {}".format(get_temp())
-    print(get_temp())
+    #print(get_temp())
     mylcd.lcd_display_string(internal, 1)
     mylcd.lcd_display_string(external, 2)
     sleep(5)

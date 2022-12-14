bf565f112b485bb8fcab2d2def3c1cf7175c5acf
Fco Javier Ruiz Rodríguez <fjruiz.1992@gmail.com>
2022-12-14 11:39:10 +0100

    Keep connection to show temperature

--- temp.py
+++ temp.py
@@ -1,17 +1,29 @@
 import I2C_LCD_driver
+from paramiko import SSHClient
 from time import *
 from subprocess import check_output
 from gpiozero import CPUTemperature
 
+client = SSHClient()
+client.load_system_host_keys()
+#client.load_host_keys("~/.ssh/known_hosts")
+#client.set_missing_host_key_policy(AutoAddPolicy())
+
+client.connect("zero2pi", username="javi")
+
 def get_temp():
     try:
-        process = check_output(['ssh', 'javi@zero2pi', 'sh', 'scripts/lm75.sh'])
-        result = process.decode('utf-8')
+        stdin, process, out = client.exec_command("sh scripts/lm75.sh")
+        result = process.read().decode('utf-8')
     except:
         result = "Error ssh"
+    stdin.close()
+    process.close()
+    out.close()
     return result
 
 mylcd = I2C_LCD_driver.lcd()
+
 
 while True:
     cpu = str(round(CPUTemperature().temperature, 1)).replace("\n", "") + " C"
